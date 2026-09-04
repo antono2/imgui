8ef68aa33f36ebda2248c2b134d1adf09a117d83
Anton Oreskin <antono2@web.de>
2026-09-04 13:39:44 +0200

    Update ImGui bindings and portable native builds

--- README.md
+++ README.md
@@ -15,11 +15,37 @@ This is an automated process to generate `imgui.v` and `implot.v`
 ## Install
 ```bash
 v install https://github.com/antono2/imgui
-# Build vimgui lib
+# Build libvimgui for this machine (without regenerating V bindings)
 cd ~/.vmodules/imgui
-cmake .
-make
+./build_vimgui.sh
 ```
+
+### Native-library choices
+
+The default uses a shared `libvimgui` and the system GLFW development package:
+
+```bash
+./build_vimgui.sh --linkage shared --glfw system
+```
+
+To build a static archive and select it from V:
+
+```bash
+./build_vimgui.sh --linkage static --glfw system
+v -d imgui_static run your_app.v
+```
+
+GLFW can instead be downloaded at a chosen release tag. This is useful for a
+reproducible application bundle:
+
+```bash
+./build_vimgui.sh --linkage shared --glfw bundled --glfw-version 3.4
+```
+
+`VIMGUI_LINKAGE`, `VIMGUI_GLFW_PROVIDER`, and `VIMGUI_GLFW_VERSION` provide the
+same choices as environment variables. System GLFW is preferable for distro
+packages. Bundled GLFW is preferable when shipping a matching `libglfw.so.3`
+beside `libvimgui.so`; use an `$ORIGIN` runtime path in the application package.
 ## Examples
 Using GLFW and Dear ImGui [antono2/v_imgui_examples](https://github.com/antono2/v_imgui_examples)
 
@@ -37,7 +63,9 @@ Or
 v generate.vsh
 ```
 
+`generate_v.sh` and `generate.vsh` regenerate both bindings and then build
+`libvimgui`. To only rebuild the native library after a system upgrade or on
+an older Linux distribution, run `./build_vimgui.sh` (or `v build_vimgui.vsh`).
+
 ## Thanks
 Thank you [@ryoskzypu](https://github.com/ryoskzypu) - from  #regex on [libera.chat](https://libera.chat/) - for loving perl and helping people out.
-
-
--- generate_v.sh
+++ generate_v.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 # Make sure the current working dir = this script dir
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pushd $SCRIPT_DIR
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+pushd "$SCRIPT_DIR" >/dev/null
 printf " --- Changed working dir to\n$SCRIPT_DIR\n\n"
 
 printf " --- Stashing CIMGUI / CIMPLOT changes"
@@ -14,19 +15,17 @@ printf " --- Stashing CIMGUI / CIMPLOT changes"
 #popd
 printf " --- Updating Submodules"
 printf "     Note: Change submodule version to change cimgui version"
-git submodule update --init --recursive
+#git submodule update --init --recursive
 #git submodule foreach git pull
 
 TARGETS_CIMGUI="internal" #"comments constructors internal noimstrv"
 TARGETS_CIMPLOT="internal"
-CFLAGS="glfw opengl3 opengl2 sdl2 sdl3"
 #DFLAGS="-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCIMGUI_DEFINE_ENUMS_AND_STRUCTS=ON -DIMGUI_STATIC=ON -DCIMGUI_NO_EXPORT=ON -DCIMGUI_USE_GLFW=ON"
 DFLAGS="-DSTATIC_BUILD=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCIMGUI_DEFINE_ENUMS_AND_STRUCTS=ON -DIMGUI_STATIC=OFF -DCIMGUI_NO_EXPORT=ON -DCIMGUI_USE_GLFW=ON"
+CFLAGS="glfw" #opengl3 opengl2 sdl2 sdl3"
 
 printf " --- Generate cimgui\n\n"
-#rm cimgui/CMakeCache.txt
-#rm cimplot/CMakeCache.txt
-#rm CMakeCache.txt
+rm -f cimgui/CMakeCache.txt cimplot/CMakeCache.txt CMakeCache.txt
 pushd cimgui/generator
   # ./generator.lua <compiler> "<targets>" <CFLAGS>
   luajit ./generator.lua gcc $TARGETS_CIMGUI $CFLAGS &> /dev/null
@@ -58,8 +57,6 @@ popd
 
 printf " --- Build cimplot\n\n"
 pushd cimplot
-#  cmake $DFLAGS $CFLAGS . &> /dev/null
-#  make &> /dev/null
 printf " --- Add ____TRANSLATIONFIX____ to cimplot.h\n\n"
 perl -p -i -g -e 's/(struct\s[\w\d]+\s\{[^\}]+(?:union\s+\{[^\}]+\};[^\}]+)?\};\s)(typedef\s(?!struct|enum)[^\n]+)/$1\n\nstruct ____TRANSLATIONFIX____;\n$2/g' cimplot.h
 popd
@@ -71,6 +68,11 @@ cp cimplot/*.h include/
 # Keep a copy at its original place. For the next run
 cp include/cimplot.h cimplot/generator/output/
 cp include/cimplot.cpp cimplot/generator/output/
+
+#printf "Remove Asserts"
+#perl -p -i -g -e 's/(IM_ASSERT\(ImGuiImplVulkanFuncs_vkCmdBeginRenderingKHR != nullptr\);)/\/\/$1/g' cimgui/imgui/backends/imgui_impl_vulkan.cpp
+#perl -p -i -g -e 's/(IM_ASSERT\(ImGuiImplVulkanFuncs_vkCmdEndRenderingKHR != nullptr\);)/\/\/$1/g' cimgui/imgui/backends/imgui_impl_vulkan.cpp
+#perl -p -i -g -e 's/(IM_ASSERT\(info->ImageCount >= info->MinImageCount\);)/\/\/$1/g' cimgui/imgui/backends/imgui_impl_vulkan.cpp
 
 printf " --- Copy imgui to ./include/imgui\nNote, ./cimgui/imgui submodule is copied, instead of ./imgui\n\n"
 pushd cimgui/imgui
@@ -85,28 +87,23 @@ popd
 pushd include
   printf " --- Translate to V\n\n"
   printf "[project]\nadditional_flags = \"$DFLAGS\"\n" > c2v.toml
-  v translate cimgui.h &> /dev/null
-  v translate cimplot.h &> /dev/null
+  v translate cimgui.h #&> /dev/null
+  v translate cimplot.h #&> /dev/null
 popd
 
 printf " --- Move implot&gui.v\n\n"
-mv include/cimplot.v src/implot.v
-mv include/cimgui.v src/imgui.v
+mv -f include/cimplot.v implot/implot.v
+mv -f include/cimgui.v imgui.v
 
-printf " --- Cleanup src/implot&gui.v\n\n"
-./cleanup_imgui.perl
-./cleanup_implot.perl
-v fmt -w src/imgui.v &> /dev/null
-v fmt -w src/implot.v &> /dev/null
-
-# Remove ~/.vmodules/imgui/imgui
-rm -rf ./imgui
-
-# Move ImPlot to its own submodule
-mv src/implot.v implot/
+printf " --- Clean generated imgui & implot bindings\n\n"
+./cleanup_imgui_implot.perl imgui.v imgui.v imgui
+./cleanup_imgui_implot.perl implot/implot.v implot/implot.v implot
+v fmt -w imgui.v
+# Do not run v fmt on the generated ImPlot binding. ImPlotSpec has a C field
+# named `Marker` whose V type is also `Marker`; the formatter currently
+# collapses `Marker Marker` to `Marker`, producing invalid V source.
 
 printf " --- Build vimgui\n\n"
-cmake $DFLAGS $CFLAGS . &> /dev/null
-make &> /dev/null
+./build_vimgui.sh
 
-popd
+popd >/dev/null
